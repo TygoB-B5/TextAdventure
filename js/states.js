7e477923b503c555cc6a5f14e6775e6ea5dc227c
TygoB-B5 <tygoboons@hotmail.nl>
2021-03-02 12:32:31 +0100

    Update states.js

--- js/states.js
+++ js/states.js
@@ -230,7 +230,7 @@ class Intro
 
             case 22:
                 Print.AsParagraph("That was all of the tutorial, now let's begin your adventure!");
-                this.FinishState();
+                player.input.state.SetActiveState(1, 0);
                 break;
 
             default:
@@ -239,11 +239,6 @@ class Intro
         }
         this.dIndex++;
         return 1;
-    }
-
-    FinishState()
-    {
-        player.input.state.SetActiveState(1, 0);
     }
 }
 
@@ -290,7 +285,7 @@ class LayDown
                 else if(Interact.Stand(msg))
                 {
                     Print.AsDialogue("You stand up and almost fall due to the strength of the wind.");
-                    this.FinishState();
+                    player.input.state.SetActiveState(2, 0);
                 }
                 else
                 {
@@ -302,14 +297,8 @@ class LayDown
                 alert("Error, dIndex out of range, " + this.dIndex);
                 break;
         }
-        
         this.dIndex++;
         return 1;
-    }
-
-    FinishState()
-    {
-        player.input.state.SetActiveState(2, 0);
     }
 }
 
@@ -318,7 +307,7 @@ class Mountain
     constructor(dIndex)
     {
         this.dIndex = dIndex;
-        this.coord = new Cord2D(1, 1);
+        this.coord = new Cord2D(2, 2);
     }
 
     Continue(msg)
@@ -329,36 +318,121 @@ class Mountain
         {
             case 0:
                 Print.AsDialogue("You found yourself standing on a tall mountain, u should try to explore a bit.");
+                this.dIndex++;
                 break;
 
             case 1:
-                if(Interact.Sit(msg) && this.coord == new Cord2D(1, 1))
+                if(Interact.Sit(msg) && (this.coord.x == 1 && this.coord.y == 1))
                 {
                     Print.AsDialogue("You lay back down in the grass");
                     player.input.state.SetActiveState(1, 0);
                 }
-                else
-                {
-                    return 0;
+
+                //#region set cords
+                else if(Navigation.IsForward(msg)) {
+                    if(this.coord.y < 3)
+                    {
+                        this.coord.y++;
+                    }
+                    else
+                    {
+                        Print.AsDialogue("I don't want to fall off the mountain!");
+                        return 1;
+                    }
                 }
+                else if (Navigation.IsRight(msg)) {
+                    if(this.coord.x < 3)
+                    {
+                        this.coord.x++;
+                    }
+                    else
+                    {
+                        Print.AsDialogue("I don't want to fall off the mountain!");
+                        return 1;
+                    }
+                }
+                else if (Navigation.IsBack(msg)) {
+                    if(this.coord.y > 0)
+                    {
+                        this.coord.y--;
+                    }
+                    else
+                    {
+                        Print.AsDialogue("I don't want to fall off the mountain!");
+                        return 1;
+                    }
+                }
+                else if (Navigation.IsLeft(msg)) {
+                    if(this.coord.x > 0)
+                    {
+                        this.coord.x--;  
+                    }
+                    else
+                    {
+                        Print.AsDialogue("I don't want to fall off the mountain!");
+                        return 1;
+                    }
+                }
+                //#endregion
+
+                //#region print output for cords
+                if(this.coord.y == 3)
+                {
+                    switch(this.coord.x)
+                    {
+                        case 1:
+                            Print.AsDialogue("U find a dead tree, it looks like there are some ants walking on it.");
+                            break;
+                        case 2:
+                            Print.AsDialogue("U stand on the edge of the cliff, thats a deep fall...");
+                            break;
+                        case 3:
+                            Print.AsDialogue("There seems to only be some tall grass here.");
+                            break;
+                    }
+                }
+                else if(this.coord.y == 2)
+                {
+                    switch(this.coord.x)
+                    {
+                        case 1:
+                            Print.AsDialogue("U find a small cabin, what could be inside?");
+                            break;
+                        case 2:
+                            Print.AsDialogue("U are back at the tall grass, your body print is still in it.");
+                            break;
+                        case 3:
+                            Print.AsDialogue("U see a wild waterfall, cool!");
+                            break;
+                    }
+                }
+                else if(this.coord.y == 1)
+                {
+                    switch(this.coord.x)
+                    {
+                        case 1:
+                            Print.AsDialogue(" The wind is unbearable here, I better head back quickly.");
+                            break;
+                        case 2:
+                            Print.AsDialogue(" U spot some rocks. Small ones, big ones, in a bunch of diffrent shapes and sizes.");
+                            break;
+                        case 3:
+                            Print.AsDialogue("U stumble upon some shorter grass and a small box lies in front of you.");
+                            break;
+                    }
+                }
+                //#endregion
+
+                Print.AsInfo(this.coord.x + " " + this.coord.y);
                 break;
 
             case 2:
-                alert("hi");
-                this.dIndex--;
                 break;
 
             default:
                 alert("Error, dIndex out of range, " + this.dIndex);
                 break;
         }
-        
-        this.dIndex++;
         return 1;
-    }
-
-    FinishState()
-    {
-        player.input.state.SetActiveState(3, 0);
     }
 }
